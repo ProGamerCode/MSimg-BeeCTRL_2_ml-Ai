80afd21033596ee83bdc9d4055c344d1d979dceb
Joe <ProGamerCode@outlook.com>
2018-02-21 06:08:03 -0800

    added local files
    bee enemys and brain template in scene

---
+++ unity-environment/Assets/Scripts/Crowd/CrowdAgent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+public class CrowdAgent : MonoBehaviour {
+       
+    public Transform target;
+
+    private UnityEngine.AI.NavMeshAgent agent;
+
+	void Start () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.speed = Random.Range(4.0f, 5.0f);
+        agent.SetDestination(target.position);
+	}
+	
+}

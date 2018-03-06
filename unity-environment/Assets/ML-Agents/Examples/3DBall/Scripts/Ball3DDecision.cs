952cfb19adcc8de75d5c6ee02a2bb507ef21021f
Joe <ProGamerCode@outlook.com>
2018-03-06 05:19:44 -0800

    Testing
    academy calculated

--- unity-environment/Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DDecision.cs
+++ unity-environment/Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DDecision.cs
@@ -8,24 +8,7 @@ public class Ball3DDecision : MonoBehaviour, Decision
     {
         if (gameObject.GetComponent<Brain>().brainParameters.actionSpaceType == StateType.continuous)
         {
-            List<float> ret = new List<float>();
-            if (state[2] < 0 || state[5] < 0)
-            {
-                ret.Add(state[5]);
-            }
-            else
-            {
-                ret.Add(state[5]);
-            }
-            if (state[3] < 0 || state[7] < 0)
-            {
-                ret.Add(-state[7]);
-            }
-            else
-            {
-                ret.Add(-state[7]);
-            }
-            return ret.ToArray();
+            return new float[4]{ 0f, 0f, 0f, 0.0f };
 
         }
         else
---
+++ unity-environment/Assets/Scripts/0BeeHaveBB/BeeAgent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeAgent : Agent
+{
+
+    
+
+
+    public override List<float> CollectState()
+	{
+		List<float> state = new List<float>();
+
+		return state;
+	}
+
+	public override void AgentStep(float[] act)
+	{
+
+	}
+    /*
+	public override void AgentReset()
+	{
+
+	}
+    */
+	public override void AgentOnDone()
+	{
+
+	}
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    
+    public override void InitializeAgent()
+    {
+        base.InitializeAgent();
+    }
+}
